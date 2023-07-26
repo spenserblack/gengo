15b2074418dd20f2af5bf756d61aea74aa9e7c06
Spenser Black <spenserblack01@gmail.com>
2023-07-26 23:51:44 +0000

    TEMP Build analyzer

--- gengo/src/languages/analyzer.rs
+++ gengo/src/languages/analyzer.rs
@@ -1,8 +1,28 @@
 /// Analyzes a language.
 use super::{Category, Language};
+use std::collections::HashSet;
+use regex::Regex;
+use std::path::Path;
+use std::ffi::{OsStr, OsString};
 
 pub struct Analyzers(Vec<Analyzer>);
-struct Analyzer {}
+
+impl Default for Analyzers {
+    /// Create a new language analyzer with default values.
+    fn default() -> Self {
+        Self(include!(concat!(env!("OUT_DIR"), "/analyzer.rs")))
+    }
+}
+pub struct Analyzer {
+    language: Language,
+    category: Category,
+    color: String,
+    extensions: HashSet<OsString>,
+    filenames: HashSet<OsString>,
+    patterns: Vec<Regex>,
+    heuristics: Vec<Regex>,
+    priority: f32,
+}
 
 impl Analyzer {
     /// Create a new language analyzer.
@@ -16,13 +36,66 @@ impl Analyzer {
         heuristics: &[&str],
         priority: f32,
     ) -> Self {
-        todo!()
+        let extensions = extensions.iter().map(|s| s.into()).collect();
+        let filenames = filenames.iter().map(|s| s.into()).collect();
+        // TODO Handle regex compile failures.
+        let patterns = patterns.iter().map(|s| Regex::new(s).unwrap()).collect();
+        let heuristics = heuristics.iter().map(|s| Regex::new(s).unwrap()).collect();
+        Self {
+            language,
+            category,
+            color: color.to_string(),
+            extensions,
+            filenames,
+            patterns,
+            heuristics,
+            priority,
+        }
+    }
+
+    pub fn matches_extension(&self, filename: &str) -> bool {
+        let extension = Path::new(filename).extension().unwrap_or_default();
+        self.extensions.contains(extension)
+    }
+
+    pub fn matches_filename(&self, filename: &str) -> bool {
+        self.filenames.contains(Path::new(filename).file_name().unwrap_or_default())
     }
 }
 
-impl Default for Analyzers {
-    /// Create a new language analyzer with default values.
-    fn default() -> Self {
-        Self(include!(concat!(env!("OUT_DIR"), "/analyzer.rs")))
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_matches_extension() {
+        let analyzer = Analyzer::new(
+            Language::PlainText,
+            Category::Prose,
+            "#000000",
+            &["txt"],
+            &[],
+            &[],
+            &[],
+            0.5,
+        );
+        assert!(analyzer.matches_extension("foo.txt"));
+        assert!(!analyzer.matches_extension("foo.rs"));
+    }
+
+    #[test]
+    fn test_matches_filename() {
+        let analyzer = Analyzer::new(
+            Language::PlainText,
+            Category::Prose,
+            "#000000",
+            &[],
+            &["LICENSE"],
+            &[],
+            &[],
+            0.5,
+        );
+        assert!(analyzer.matches_filename("LICENSE"));
+        assert!(!analyzer.matches_filename("Dockerfile"));
     }
 }
