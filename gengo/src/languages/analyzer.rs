ced61a9f82b8357aa0519eb7755fae44af5959e8
Spenser Black <spenserblack01@gmail.com>
2023-07-28 23:30:48 +0000

    Reimplement matchers

--- gengo/src/languages/analyzer.rs
+++ gengo/src/languages/analyzer.rs
@@ -29,7 +29,7 @@ impl Default for Analyzers {
                     category: args.category,
                     color: args.color,
                 };
-                let matcher = args.matchers.into();
+                let matchers = args.matchers.into();
                 let heuristics = args
                     .heuristics
                     .into_iter()
@@ -37,7 +37,7 @@ impl Default for Analyzers {
                     .collect();
                 Analyzer {
                     language,
-                    matcher,
+                    matchers,
                     heuristics,
                     priority: args.priority,
                 }
@@ -50,9 +50,26 @@ impl Default for Analyzers {
 /// Used to match a programming language.
 struct Analyzer {
     language: Language,
-    matcher: FilepathMatcher,
+    matchers: Vec<Matcher>,
     heuristics: Vec<Regex>,
     priority: f32,
+}
+
+trait MatcherTrait {
+    fn matches(&self, filename: &OsStr, contents: &[u8]) -> bool;
+}
+
+/// Checks if a file matches.
+enum Matcher {
+    Filepath(FilepathMatcher),
+}
+
+impl MatcherTrait for Matcher {
+    fn matches(&self, filename: &OsStr, contents: &[u8]) -> bool {
+        match self {
+            Matcher::Filepath(matcher) => matcher.matches(filename, contents),
+        }
+    }
 }
 
 /// Matches a file path.
@@ -78,24 +95,31 @@ impl FilepathMatcher {
         }
     }
 
-    pub fn matches(&self, filename: &str) -> bool {
-        self.matches_extension(filename)
-            || self.matches_filename(filename)
-            || self.matches_pattern(filename)
-    }
-
-    pub fn matches_extension(&self, filename: &str) -> bool {
+    pub fn matches_extension(&self, filename: &OsStr) -> bool {
         let extension = Path::new(filename).extension().unwrap_or_default();
         self.extensions.contains(extension)
     }
 
-    pub fn matches_filename(&self, filename: &str) -> bool {
+    pub fn matches_filename(&self, filename: &OsStr) -> bool {
         self.filenames
             .contains(Path::new(filename).file_name().unwrap_or_default())
     }
 
-    pub fn matches_pattern(&self, filename: &str) -> bool {
+    pub fn matches_pattern(&self, filename: &OsStr) -> bool {
+        let filename = if let Some(filename) = filename.to_str() {
+            filename
+        } else {
+            return false;
+        };
         self.patterns.iter().any(|p| p.is_match(filename))
+    }
+}
+
+impl MatcherTrait for FilepathMatcher {
+    fn matches(&self, filename: &OsStr, _contents: &[u8]) -> bool {
+        self.matches_extension(&filename)
+            || self.matches_filename(&filename)
+            || self.matches_pattern(&filename)
     }
 }
 
@@ -124,6 +148,23 @@ struct AnalyzerArgMatchers {
     patterns: Vec<String>,
 }
 
+impl From<AnalyzerArgMatchers> for Vec<Matcher> {
+    fn from(matchers: AnalyzerArgMatchers) -> Self {
+        let filepath_matcher = if !matchers.extensions.is_empty()
+            || !matchers.filenames.is_empty()
+            || !matchers.patterns.is_empty()
+        {
+            Some(Matcher::Filepath(matchers.into()))
+        } else {
+            None
+        };
+        [filepath_matcher]
+            .into_iter()
+            .filter_map(|m| m)
+            .collect()
+    }
+}
+
 impl From<AnalyzerArgMatchers> for FilepathMatcher {
     fn from(matchers: AnalyzerArgMatchers) -> Self {
         Self::new(
@@ -141,23 +182,23 @@ mod tests {
     #[test]
     fn test_matches_extension() {
         let analyzer = FilepathMatcher::new(&["txt"], &[], &[]);
-        assert!(analyzer.matches_extension("foo.txt"));
-        assert!(!analyzer.matches_extension("foo.rs"));
+        assert!(analyzer.matches_extension(OsStr::new("foo.txt")));
+        assert!(!analyzer.matches_extension(OsStr::new("foo.rs")));
     }
 
     #[test]
     fn test_matches_filename() {
         let analyzer = FilepathMatcher::new(&[], &["LICENSE"], &[]);
-        assert!(analyzer.matches_filename("LICENSE"));
-        assert!(!analyzer.matches_filename("Dockerfile"));
+        assert!(analyzer.matches_filename(OsStr::new("LICENSE")));
+        assert!(!analyzer.matches_filename(OsStr::new("Dockerfile")));
     }
 
     #[test]
     fn test_matches_pattern() {
         let analyzer =
             FilepathMatcher::new::<&str>(&[], &[], &[r"^Makefile(?:\.[\w\d]+)?$".into()]);
-        assert!(analyzer.matches_pattern("Makefile"));
-        assert!(analyzer.matches_pattern("Makefile.in"));
-        assert!(!analyzer.matches_pattern("Cakefile"));
+        assert!(analyzer.matches_pattern(OsStr::new("Makefile")));
+        assert!(analyzer.matches_pattern(OsStr::new("Makefile.in")));
+        assert!(!analyzer.matches_pattern(OsStr::new("Cakefile")));
     }
 }
