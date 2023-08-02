844ac227efd4c45990159767ba59146e2bdc01c0
Spenser Black <spenserblack01@gmail.com>
2023-08-02 11:33:58 -0400

    Fix typo

--- gengo-bin/src/cli.rs
+++ gengo-bin/src/cli.rs
@@ -22,7 +22,7 @@ pub struct Cli {
 
 impl Cli {
     pub fn run<W: Write>(&self, mut w: W) -> Result<(), Box<dyn Error>> {
-        // TODO Implement `Dispay` and read the `to_string` value?
+        // TODO Implement `Display` and read the `to_string` value?
         let result = gengo::add(self.left, self.right);
         writeln!(
             w,
