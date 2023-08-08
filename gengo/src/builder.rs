e048abfedb569ab65bd0d90976fb443e94975389
Spenser Black <spenserblack01@gmail.com>
2023-08-08 16:04:26 -0400

    Create `Builder`

---
+++ gengo/src/builder.rs
@@ -0,0 +1,41 @@
+
+use super::Gengo;
+use super::Analyzers;
+use std::path::Path;
+use git2::Repository;
+use std::error::Error;
+
+/// Builds a new `Gengo` instance.
+///
+/// # Example
+///
+/// ```no_run
+/// use gengo::Builder;
+/// let gengo = Builder::new("path/to/repo").build().unwrap();
+/// ```
+pub struct Builder<P: AsRef<Path>> {
+    repository_path: P,
+    analyzers: Option<Analyzers>,
+}
+
+impl<P: AsRef<Path>> Builder<P> {
+    pub fn new(repository_path: P) -> Self {
+        Self {
+            repository_path,
+            analyzers: None,
+        }
+    }
+
+    /// Sets the `Analyzers` to use. If this is not set,
+    /// `Analyzers::default()` will be used.
+    pub fn analyzers(mut self, analyzers: Analyzers) -> Self {
+        self.analyzers = Some(analyzers);
+        self
+    }
+
+    pub fn build(self) -> Result<Gengo, Box<dyn Error>> {
+        let repository = Repository::open(self.repository_path)?;
+        let analyzers = self.analyzers.unwrap_or_default();
+        Ok(Gengo { repository, analyzers })
+    }
+}
--- gengo/src/lib.rs
+++ gengo/src/lib.rs
@@ -1,3 +1,15 @@
-pub mod languages;
+use git2::Repository;
+use std::path::Path;
+use std::error::Error;
 pub use languages::analyzer::Analyzers;
 pub use languages::Language;
+pub use builder::Builder;
+
+pub mod languages;
+mod builder;
+
+/// The main entry point for Gengo.
+pub struct Gengo {
+    repository: Repository,
+    analyzers: Analyzers,
+}
