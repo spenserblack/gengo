752a42f39c8db808a6e38532ac873f1f32ecffa7
Spenser Black <spenserblack01@gmail.com>
2023-08-10 13:23:25 -0400

    chore: Skip snapshot tests on Windows
    Tests fails because separators are different. TODO added for a Windows
    user to come and help with Windows-specific snapshot tests.

--- gengo/tests/gengo_test.rs
+++ gengo/tests/gengo_test.rs
@@ -5,6 +5,7 @@ const ROOT: &str = env!("CARGO_MANIFEST_DIR");
 mod util;
 
 #[test]
+#[cfg_attr(target_os = "windows", ignore)]
 fn test_javascript() {
     let analyzers = fixture_str!("test_javascript-analyzers.yaml");
     let analyzers = Analyzers::from_yaml(analyzers).unwrap();
@@ -12,3 +13,5 @@ fn test_javascript() {
     let results = gengo.analyze("test/javascript").unwrap();
     insta::assert_debug_snapshot!(results);
 }
+
+// TODO Add test_javascript_windows
