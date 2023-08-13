60ba3491f788a22c3dda66f10bdb34df68f82d69
o2sh <ossama-hjaji@live.fr>
2023-08-13 12:15:12 +0200

    review

--- gengo/src/generated.rs
+++ gengo/src/generated.rs
@@ -1,4 +1,4 @@
-use std::{ffi::OsStr, path::MAIN_SEPARATOR};
+use std::{ffi::OsStr, path::Path};
 
 pub struct Generated;
 
@@ -8,11 +8,35 @@ impl Generated {
     }
 
     fn is_generated_no_read(filepath: &OsStr) -> bool {
-        let prefix = format!("dist{}", MAIN_SEPARATOR);
-        filepath.to_str().unwrap_or_default().starts_with(&prefix)
+        Path::new(filepath)
+            .components()
+            .next()
+            .map_or(false, |c| c.as_os_str() == "dist")
     }
 
     fn is_generated_with_read(_filepath: &OsStr, _contents: &[u8]) -> bool {
         false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest(
+        filepath,
+        expected,
+        case("dist/something.js", true),
+        case("src/something.rs", false),
+        case("dist/subfolder/something.js", true),
+        case("", false),
+        case("dist", true)
+    )]
+    fn test_is_generated_no_read(filepath: &str, expected: bool) {
+        assert_eq!(
+            Generated::is_generated_no_read(OsStr::new(filepath)),
+            expected
+        );
     }
 }
--- gengo/src/vendored.rs
+++ gengo/src/vendored.rs
@@ -1,4 +1,4 @@
-use std::{ffi::OsStr, path::MAIN_SEPARATOR};
+use std::{ffi::OsStr, path::Path};
 
 pub struct Vendored;
 
@@ -8,11 +8,35 @@ impl Vendored {
     }
 
     fn is_vendored_no_read(filepath: &OsStr) -> bool {
-        let prefix = format!("node_modules{}", MAIN_SEPARATOR);
-        filepath.to_str().unwrap_or_default().starts_with(&prefix)
+        Path::new(filepath)
+            .components()
+            .next()
+            .map_or(false, |c| c.as_os_str() == "node_modules")
     }
 
     fn is_vendored_with_read(_filepath: &OsStr, _contents: &[u8]) -> bool {
         false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest(
+        filepath,
+        expected,
+        case("node_modules/library/something.js", true),
+        case("src/something.rs", false),
+        case("node_modules/subfolder/something.js", true),
+        case("", false),
+        case("node_modules", true)
+    )]
+    fn test_is_vendored_no_read(filepath: &str, expected: bool) {
+        assert_eq!(
+            Vendored::is_vendored_no_read(OsStr::new(filepath)),
+            expected
+        );
     }
 }
