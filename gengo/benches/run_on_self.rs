ba259fdb87bfc5c3075dd86cd812c6bbdd313ad3
Spenser Black <spenserblack01@gmail.com>
2023-08-15 08:45:55 -0400

    Add benchmark

---
+++ gengo/benches/run_on_self.rs
@@ -0,0 +1,10 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use gengo::Builder;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let gengo = Builder::new(env!("CARGO_MANIFEST_DIR")).build().unwrap();
+    c.bench_function("run on self", |b| b.iter(|| gengo.analyze(black_box("HEAD"))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
