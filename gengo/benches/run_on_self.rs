80930ad99133e7fff934b32e58b7907abac7c696
Spenser Black <spenserblack01@gmail.com>
2023-08-15 09:00:53 -0400

    Run benchmark on last 3 revs

--- gengo/benches/run_on_self.rs
+++ gengo/benches/run_on_self.rs
@@ -1,12 +1,15 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use gengo::Builder;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn head_benchmark(c: &mut Criterion) {
     let gengo = Builder::new(env!("CARGO_MANIFEST_DIR")).build().unwrap();
-    c.bench_function("run on self", |b| {
-        b.iter(|| gengo.analyze(black_box("HEAD")))
-    });
+    for n in 0..3 {
+        let rev = format!("HEAD{}", "^".repeat(n));
+        c.bench_function(&format!("run on {}", rev), |b| {
+            b.iter(|| gengo.analyze(black_box(&rev)).unwrap())
+        });
+    }
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, head_benchmark);
 criterion_main!(benches);
