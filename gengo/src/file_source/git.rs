ea531adcc674a146b431b7f5260d0a5ed179fc98
Spenser Black <spenserblack01@gmail.com>
2023-09-21 11:47:06 -0400

    Set up overrides for git file source

--- gengo/src/file_source/git.rs
+++ gengo/src/file_source/git.rs
@@ -1,4 +1,4 @@
-use super::{FileSource, Result};
+use super::{FileSource, Result, Overrides};
 use gix::{
     attrs::StateRef,
     bstr::{BString, ByteSlice},
@@ -17,19 +17,13 @@ use std::path::Path;
 use std::borrow::Cow;
 use std::error::Error as ErrorTrait;
 
-pub struct Git {
+struct Builder {
     repository: ThreadSafeRepository,
     rev: String,
 }
 
-impl Git {
-    const OVERRIDE_ATTRS: [&'static str;5] = ["gengo-language", "gengo-generated", "gengo-documentation", "gengo-vendored", "gengo-detectable"];
-    const LANGUAGE_OVERRIDE: usize = 0;
-    const GENERATED_OVERRIDE: usize = 1;
-    const DOCUMENTATION_OVERRIDE: usize = 2;
-    const VENDORED_OVERRIDE: usize = 3;
-    const DETECTABLE_OVERRIDE: usize = 4;
-    pub fn new<P: AsRef<Path>>(path: P, rev: &str) -> Result<Self> {
+impl Builder {
+    fn new<P: AsRef<Path>>(path: P, rev: &str) -> Result<Self> {
         let repository = match gix::discover(path) {
             Ok(r) => r,
             Err(DiscoverError::Discover(err)) => {
@@ -49,7 +43,7 @@ impl Git {
         let tree_id = repo.rev_parse_single(self.rev.as_str())?.object()?.peel_to_tree()?.id;
         let index = repo.index_from_tree(&tree_id)?;
         let attr_stack = repo.attributes_only(&index, AttrSource::IdMapping)?;
-        let attr_matches = attr_stack.selected_attribute_matches(Self::OVERRIDE_ATTRS);
+        let attr_matches = attr_stack.selected_attribute_matches(Git::OVERRIDE_ATTRS);
         let (index_state, _) = index.into_parts();
         let attr_stack = attr_stack.detach();
         let state = State {
@@ -58,6 +52,34 @@ impl Git {
             index_state,
         };
         Ok(state)
+    }
+
+    fn build(self) -> Result<Git> {
+        let state = self.state()?;
+        let git = Git {
+            repository: self.repository,
+            rev: self.rev,
+            state,
+        };
+        Ok(git)
+    }
+}
+
+pub struct Git {
+    repository: ThreadSafeRepository,
+    rev: String,
+    state: State,
+}
+
+impl Git {
+    const OVERRIDE_ATTRS: [&'static str;5] = ["gengo-language", "gengo-documentation", "gengo-generated", "gengo-vendored", "gengo-detectable"];
+    const LANGUAGE_OVERRIDE: usize = 0;
+    const DOCUMENTATION_OVERRIDE: usize = 1;
+    const GENERATED_OVERRIDE: usize = 2;
+    const VENDORED_OVERRIDE: usize = 3;
+    const DETECTABLE_OVERRIDE: usize = 4;
+    pub fn new<P: AsRef<Path>>(path: P, rev: &str) -> Result<Self> {
+        Builder::new(path, rev)?.build()
     }
 }
 
@@ -68,18 +90,69 @@ impl<'repo> FileSource<'repo> for Git {
 
     fn files(&self) -> Result<Self::Iter> {
         todo!("Initialize iterator");
-        let state = self.state()?;
         let iter = Iter {
-            state,
             stack: (),
             foo: & (),
         };
         Ok(iter)
     }
+
+    fn overrides<O: AsRef<Path>>(
+        &mut self,
+        path: O,
+    ) -> Overrides {
+        let repo = self.repository.to_thread_local();
+        let platform = self.state.attr_stack.at_path(path, Some(false), |id, buf| {
+            repo.objects.find_blob(id, buf)
+        });
+        // NOTE If we cannot get overrides, simply don't return them.
+        let platform = match platform {
+            Ok(platform) => platform,
+            Err(_) => return Default::default(),
+        };
+        platform.matching_attributes(&mut self.state.attr_matches);
+
+        let attrs = {
+            let mut attrs = [None, None, None, None, None];
+            self.state.attr_matches.iter_selected().zip(attrs.iter_mut()).for_each(|(info, slot)| {
+                *slot = (info.assignment.state != StateRef::Unspecified).then_some(info);
+            });
+            attrs
+        };
+
+        let language = attrs[Self::LANGUAGE_OVERRIDE]
+            .as_ref()
+            .and_then(|info| match info.assignment.state {
+                StateRef::Value(v) => v.as_bstr().to_str().ok().map(|s| s.replace('-', " ")),
+                _ => None,
+            });
+        // NOTE Unspecified attributes are None, so `state.is_set()` is
+        //      implicitly `!state.is_unset()`.
+        // TODO This is really repetitive. Refactor to iteration?
+        let is_documentation = attrs[Self::DOCUMENTATION_OVERRIDE]
+            .as_ref()
+            .map(|info| info.assignment.state.is_set());
+        let is_generated = attrs[Self::GENERATED_OVERRIDE]
+            .as_ref()
+            .map(|info| info.assignment.state.is_set());
+        let is_vendored = attrs[Self::VENDORED_OVERRIDE]
+            .as_ref()
+            .map(|info| info.assignment.state.is_set());
+        let is_detectable = attrs[Self::DETECTABLE_OVERRIDE]
+            .as_ref()
+            .map(|info| info.assignment.state.is_set());
+
+        Overrides {
+            language,
+            is_documentation,
+            is_generated,
+            is_vendored,
+            is_detectable,
+        }
+    }
 }
 
 pub struct Iter<'repo> {
-    state: State,
     stack: (),
     foo: &'repo (),
 }
--- gengo/src/file_source/mod.rs
+++ gengo/src/file_source/mod.rs
@@ -18,6 +18,25 @@ pub trait FileSource<'contents> {
     /// Returns an iterator over the files.
     fn files(&self) -> Result<Self::Iter>;
 
+    /// Provides combined overrides for the file.
+    fn overrides<O: AsRef<Path>>(
+        &mut self,
+        path: O,
+    ) -> Overrides {
+        Overrides {
+            language: self.language_override(&path),
+            is_documentation: self.is_documentation_override(&path),
+            is_generated: self.is_generated_override(&path),
+            is_vendored: self.is_vendored_override(&path),
+            is_detectable: self.is_detectable_override(&path),
+        }
+    }
+
+    /// Provides an optional override for the detected language.
+    fn language_override<O: AsRef<Path>>(&self, _path: O) -> Option<String> {
+        None
+    }
+
     /// Provides an optional override for documentation file detection.
     fn is_documentation_override<O: AsRef<Path>>(&self, _path: O) -> Option<bool> {
         None
@@ -33,8 +52,18 @@ pub trait FileSource<'contents> {
         None
     }
 
-    /// Provides an optional override for the detected language.
-    fn language_override<O: AsRef<Path>>(&self, _path: O) -> Option<Language> {
+    /// Provides an optional override for if the file is detectable.
+    fn is_detectable_override<O: AsRef<Path>>(&self, _path: O) -> Option<bool> {
         None
     }
+}
+
+#[non_exhaustive]
+#[derive(Default)]
+pub struct Overrides {
+    pub language: Option<String>,
+    pub is_documentation: Option<bool>,
+    pub is_generated: Option<bool>,
+    pub is_vendored: Option<bool>,
+    pub is_detectable: Option<bool>,
 }
