e5afacc4e0fe844da751040b9ea1b794531275ee
Spenser Black <spenserblack01@gmail.com>
2023-09-26 08:49:41 -0400

    Benchmark Gengo on `test/javascript`

--- gengo/benches/run_on_self.rs
+++ gengo/benches/run_on_self.rs
@@ -3,8 +3,12 @@ use gengo::Builder;
 
 fn head_benchmark(c: &mut Criterion) {
     let gengo = Builder::new(env!("CARGO_MANIFEST_DIR")).build().unwrap();
-    for n in 0..3 {
-        let rev = format!("HEAD{}", "^".repeat(n));
+    let revs = {
+        let mut revs: Vec<_> = (0..3).map(|n| format!("HEAD{}", "^".repeat(n))).collect();
+        revs.extend_from_slice(&["test/javascript".into()]);
+        revs
+    };
+    for rev in revs {
         c.bench_function(&format!("run on {}", rev), |b| {
             b.iter(|| gengo.analyze(black_box(&rev)).unwrap())
         });
