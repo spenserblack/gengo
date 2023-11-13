2ce08fa697f9220091490eb1b29baf29ae385036
Spenser Black <spenserblack01@gmail.com>
2023-11-13 12:10:55 -0500

    Update `install.sh` for new release asset names

--- install.sh
+++ install.sh
@@ -12,10 +12,13 @@ echo "Installing gengo to $INSTALL_DIR"
 echo "This script will activate sudo to install to $INSTALL_DIR"
 sudo echo "sudo activated"
 
+# TODO Handle other architectures
 if [ "$OS_NAME" = "Linux" ]; then
 	OS="Linux"
+	BUILD_TARGET="x86_64-unknown-linux-gnu"
 elif [ "$OS_NAME" = "Darwin" ]; then
 	OS="macOS"
+	BUILD_TARGET="x86_64-apple-darwin"
 else
 	NO_RELEASE_ASSET="true"
 	echo "There is no Unix release for this OS: $OS_NAME" >&2
@@ -24,13 +27,11 @@ fi
 if [ "$ARCH_NAME" != "x86_64" ]; then
 	NO_RELEASE_ASSET="true"
 	echo "There is no release for this architecture: $ARCH_NAME" >&2
-else
-	ARCH="X64"
 fi
 
 if [ "$NO_RELEASE_ASSET" ]; then
 	exit 1
 fi
 
-curl -sSL "https://github.com/spenserblack/gengo/releases/latest/download/gengo-$OS-$ARCH.tar.gz" | sudo tar -C "$INSTALL_DIR" -xzf -
+curl -sSL "https://github.com/spenserblack/gengo/releases/latest/download/gengo-$BUILD_TARGET.tar.gz" | sudo tar -C "$INSTALL_DIR" -xzf -
 echo "successfully installed gengo"
