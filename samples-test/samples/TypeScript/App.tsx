e4c7cd49553cb6fcd7e07e0ac7a95c0b20bed112
ANKDDEV <andrekabatareika@gmail.com>
2025-02-20 16:53:49 +0300

    Add TS samples

---
+++ samples-test/samples/TypeScript/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to My React App</h1>
+                <p>
+                    Start editing to see some magic happen!
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
