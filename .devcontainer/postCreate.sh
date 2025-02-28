f33f7e6fda4fc7f988514cc44bc1bd82b3aa62ba
Spenser Black <spenserblack01@gmail.com>
2025-02-28 13:43:01 -0500

    Add test branch before installing binaries

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cargo install --locked cargo-insta taplo-cli
-
 git remote add gengo https://github.com/spenserblack/gengo
 git fetch gengo test/javascript:test/javascript
+
+cargo install --locked cargo-insta taplo-cli
