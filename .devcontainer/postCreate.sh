fc954cb0e35f38693db99dc0ba54cb9f108e0f51
ANKDDEV <andrekabatareika@gmail.com>
2025-02-28 06:46:50 +0300

    feat(devcontainer): call `git fetch origin/upstream test/javascript:test/javascript` when creating devcontainer (spenserblack#138)

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 cargo install --locked cargo-insta taplo-cli
+
+# Get remote URL
+origin=$(git config --get remote.origin.url)
+
+if [ "$origin" = "https://github.com/spenserblack/gengo" ]; then
+  git fetch origin test/javascript:test/javascript
+else
+  git fetch upstream test/javascript:test/javascript
+fi
